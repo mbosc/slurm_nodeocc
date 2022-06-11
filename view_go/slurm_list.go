cbca181801c2926c20e1c72c8b3b252a5631b30b
Matteo Boschini <matteo.boschini@unimore.it>
2022-06-11 23:57:29 +0200

    added xterm color spec

--- view_go/slurm_list.go
+++ view_go/slurm_list.go
@@ -13,6 +13,7 @@ import (
 )
 
 func main() {
+	os.Setenv("TERM", "xterm-256color")
 	err := tea.NewProgram(initialModel(), tea.WithAltScreen()).Start()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
